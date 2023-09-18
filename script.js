69fbc2e9523f334a680b1fdcf348e02390c3f841
Abat Annamammedova <117486085+Abat01@users.noreply.github.com>
2023-09-18 12:10:13 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+//-----------------Sections type-----
+const homePage = document.querySelector('');
+const imgPickPage = document.querySelector('');
+const puzzlePage = document.querySelector('');
+
+//--------Buttons clickables--------------
+const startBtn = document.querySelector('');
+const continueBtn = document.querySelector('');
+const dropdownBtn = document.querySelector('');
+const shuffleBtn = document.querySelector('');
+
+//----------Images shuffles/dropdown---------
+
+
+//----------Slider uchin--------------------
